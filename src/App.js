bc37afd67b728edbaa0a18a6b3f82e3e3822ce66
user00777777 <misterborman@gmail.com>
2022-07-18 21:26:06 +0300

    duty

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Nav from "./Redusers/Components/Navigation/Nav";
 import Header from "./Redusers/Components/Header/Header";
-import React from "react";
+
 // import PostComponent from "./Redusers/Components/Content/PostComponent";
 import OndutynowComponent from "./Redusers/Components/OnDutyNow/OndutynowComponent";
+import PostComponent from "./Redusers/Components/Content/PostComponent";
 
 function App(props) {
   return (
@@ -15,7 +16,8 @@ function App(props) {
 
       <div className="wrapper-content">
         <Routes>
-          {/* <Route path="/Content" element={<PostComponent />} /> */}
+          ``
+          <Route path="/Content" element={<PostComponent />} />
           <Route path="/OnDutyNow" element={<OndutynowComponent />} />
         </Routes>
       </div>
